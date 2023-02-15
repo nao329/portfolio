e0d70ffc83e48afcafd05b8a0fdb9d29899f3a84
nao329 <naoya5151@icloud.com>
2023-02-16 02:28:34 +0900

    修正

--- index.js
+++
@@ -1,9 +0,0 @@
-import { Server } from "https://code4sabae.github.io/js/Server.js";
-
-class MyServer extends Server {
-  api(path, req) {
-    const time = new Date();
-    return { path, req,  time };
-  }
-}
-new MyServer(8881);
